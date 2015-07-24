5473aca0ef9ed04ee4ce02cd52d5147c147050df
st1x <servis.2012@yandex.ru>
2015-07-24 15:52:25 +0300

    6.2

--- src/Main.java
+++ src/Main.java
@@ -74,6 +74,7 @@ public class Main {
         System.out.println(char1.substring(1, char1.length()));
         System.out.println();
 
+
         //target
         System.out.print("Normal string: \"");
         System.out.println(main1.target + "\"");
